037dcd4091d7b95fe942aac73f5a78d426ffb317
Shamsul <48136141+shkms11@users.noreply.github.com>
2025-12-24 03:16:13 +0600

    add a draft template

---
+++ template-draft01.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    
+
+}
+//g++ -o code code.cpp & code.exe <in.txt > out.txt

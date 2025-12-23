dac089779b2016c6d8d5086d06767af18fbb1cb1
Shamsul <48136141+shkms11@users.noreply.github.com>
2025-12-24 03:24:07 +0600

    update fastio using define

--- template-draft01.cpp
+++ template-draft01.cpp
@@ -1,14 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define MOD 1000000007
 #define MOD1 998244353
 #define INF 1e18
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
+    // ios::sync_with_stdio(false);
+    // cin.tie(0);
+    fastio();
     
 
 }
